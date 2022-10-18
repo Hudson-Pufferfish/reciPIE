f7cb255181e6c90d9b7c3e2fa35d1a8be98b76a5
millie178 <slwangpwa@gmail.com>
2022-10-18 12:18:58 -0500

    recipie api view

---
+++ src/js/config.js
@@ -0,0 +1,23 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e8351382-2296-44a7-bdf6-a75de0131b6a';
+export const MODAL_CLOSE_SEC = 2.5;
+
+export const CLIENT_ID = '347847861216-3se11mbfeteegu3i9mrlbhfnqokhveng.apps.googleusercontent.com';
+export const CLIENT_SECRET = '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';
+export const REDIRECT_URL = 'https://developers.google.com/oauthplayground';
+export const REFRESH_TOKEN = 'ya29.a0Aa4xrXP-xU6VNs_0AW097sQcqKjtsaaSLfs1UxvBkkOLRWPeIhZ3cMmza-zZUwA_Nmk75JIlGFJz1nPV4pmpUkHgJQb8vpSbJmXdq_6tcuK2Gxd20-nTRst6cZxcH_y84JVrevNEbhe9qRWUgSjVAkhmGo3taCgYKATASARASFQEjDvL9VnqxlP0rCR5jWLo7G5aQxg0163';
+
+// https://forkify-api.herokuapp.com/v2
+/*
+https://developers.google.com/oauthplayground/
+oauth client id:
+347847861216-3se11mbfeteegu3i9mrlbhfnqokhveng.apps.googleusercontent.com
+
+oauth client secret:
+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
+
+api key:
+AIzaSyAsuuPuGDVoMdFj6-AwWPSPhB6xIfGHJ5g
+*/
