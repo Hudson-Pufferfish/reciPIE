ca54e8a4a65eb075cb0fa83b21d15d23163b98d1
Hudson Nguyen <hungson.nguyen811@email.com>
2022-11-08 14:32:35 -0600

    modify mashup again

--- src/js/config.js
+++
@@ -1,7 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = 'e8351382-2296-44a7-bdf6-a75de0131b6a';
-export const MODAL_CLOSE_SEC = 2.5;
-
-
